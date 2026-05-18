66ecef261346f4d09e64e3ee7e96ac7dd3572fa7
Evelyn <eve.mora@duocuc.cl>
2026-05-17 20:39:10 -0400

    Feat: Datos de prueba de cursos cargados con éxito

--- src/main/java/com/colegio/mscurso/Application.java
+++ src/main/java/com/colegio/mscurso/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = "com.colegio.mscursos")
-@EnableFeignClients(basePackages = "com.colegio.mscursos") // <-- Esto activa la magia de Feign
+@SpringBootApplication(scanBasePackages = "com.colegio.mscurso")
+@EnableFeignClients(basePackages = "com.colegio.mscurso")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/colegio/mscurso/client/EstudianteClient.java
+++ src/main/java/com/colegio/mscurso/client/EstudianteClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // Aquí le dices a Spring que apunte al puerto de tu ms-estudiantes (ej: 8081)
-@FeignClient(name = "ms-estudiantes", url = "http://localhost:8081/api/estudiantes")
+@FeignClient(name = "ms-estudiantes", url = "http://localhost:8081/api/v1/estudiantes")
 public interface EstudianteClient {
 
     @GetMapping("/{id}")
